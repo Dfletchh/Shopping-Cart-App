f37af35460a166e37e28edebf70e5ddab2eea465
Dean Fletcher <maxfletch101@yahoo.com>
2023-02-02 16:57:10 -0500

    Custom useCart Hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,21 @@
 import Nav from "./Nav"
+import useCart from "../hooks/useCart"
 
 type PropsType = {
   viewCart: boolean
   setViewCart: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const header = ({ viewCart, setViewCart }: PropsType) => {
+const Header = ({ viewCart, setViewCart }: PropsType) => {
+  const { totalItems, totalPrice } = useCart()
+
   const content = (
     <header className='header'>
       <div className='header__title-bar'>
         <h1>The Coral Collective</h1>
         <div className='header__price-box'>
-          <p>Total Items:</p>
-          <p>Total Price:</p>
+          <p>Total Items: {totalItems}</p>
+          <p>Total Price: {totalPrice}</p>
         </div>
         <Nav viewCart={viewCart} setViewCart={setViewCart} />
       </div>
@@ -22,4 +25,4 @@ const header = ({ viewCart, setViewCart }: PropsType) => {
   return <div>header</div>
 }
 
-export default header
+export default Header
