03e57c9d2365001ecf713ac4dfccdcc1a3762260
Dean Fletcher <maxfletch101@yahoo.com>
2023-02-02 16:51:14 -0500

    Nav button added to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,24 @@
+import Nav from "./Nav"
+
 type PropsType = {
   viewCart: boolean
   setViewCart: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const header = ({ viewCart, setViewCart }: PropsType) => {
-  const content = {}
+  const content = (
+    <header className='header'>
+      <div className='header__title-bar'>
+        <h1>The Coral Collective</h1>
+        <div className='header__price-box'>
+          <p>Total Items:</p>
+          <p>Total Price:</p>
+        </div>
+        <Nav viewCart={viewCart} setViewCart={setViewCart} />
+      </div>
+    </header>
+  )
+
   return <div>header</div>
 }
 
