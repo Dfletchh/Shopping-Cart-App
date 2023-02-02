e0efa318d202d15202d1e86a04ba9e753c1bc4cf
Dean Fletcher <maxfletch101@yahoo.com>
2023-02-02 17:04:37 -0500

    footer content finished

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import useCart from "../hooks/useCart"
 
-const footer = () => {
-  return <div>footer</div>;
-};
+type PropsType = { viewCart: boolean }
 
-export default footer;
+const footer = ({ viewCart }: PropsType) => {
+  const { totalItems, totalPrice } = useCart()
+
+  const year: number = new Date().getFullYear()
+
+  const pageContent = viewCart ? (
+    <p>Shopping Cart &copy; {year}</p>
+  ) : (
+    <>
+      <p>Total Items: {totalItems}</p>
+      <p>Total Price: {totalPrice}</p>
+      <p>Shopping Cart &copy; {year}</p>
+    </>
+  )
+
+  const content = <footer className='footer'>{pageContent}</footer>
+  return content
+}
+
+export default footer
